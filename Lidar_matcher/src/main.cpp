2bf1f809c29dd89ec8ea3a386f6750b1e5a74dc4
Kasper Hoej Lorenzen <kasper.lorenzen@gmail.com>
2019-05-06 11:25:52 +0200

    Lidar matcher getting started

--- Lidar_matcher/src/main.cpp
+++ Lidar_matcher/src/main.cpp
@@ -10,16 +10,25 @@
 #include <inspec_lib/settings/SettingStructs.hpp>
 #include <inspec_lib/settings/ReadSettings.hpp>
 
+#include <deque>
+#include <vector>
+
 using namespace std;
 
 ros::Publisher Matched_pub;
 settings::Camera camera_setting;
+settings::Lidar_matcher setting;
+
+deque<inspec_msg::lidardat> incoming_data;
 
 void Lidar_data_handler(inspec_msg::lidardat msg){
-
+    incoming_data.push_back(msg);
 }
 void Line_handler(inspec_msg::line2d_array msg){
-
+    if(!incoming_data.empty()){
+        double last_dif = msg.header.stamp.toSec() - incoming_data.front().header.stamp.toSec();
+    }
+    
 }
 
 
@@ -27,6 +36,7 @@ int main(int argc, char **argv){
     ros::init(argc,argv,"Lidar_matcher_node");
     ros::NodeHandle nh = ros::NodeHandle();
     settings::read(camera_setting);
+    settings::read(setting);
 
     ros::Subscriber lidar_data_sub = nh.subscribe("/lidarDat",1,Lidar_data_handler);
     ros::Subscriber line_sub = nh.subscribe("/linedetector/lines2d",1,Line_handler);
